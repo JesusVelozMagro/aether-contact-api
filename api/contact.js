ff8eb2f872db4512ca5bb2aa490e5c87f3ac4e2c
JesusVelozMagro <117239674+JesusVelozMagro@users.noreply.github.com>
2025-04-16 13:22:24 -0400

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,40 +1,60 @@
-//import nodemailer from 'nodemailer';
+// api/contact.js
+export default async function handler(req, res) {
+  // Habilitar CORS
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-//export default async function handler(req, res) {
-  //res.setHeader("Access-Control-Allow-Origin", "*");
-  //res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  //res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // preflight
+  }
+//Sólo para testeo
 
-  //if (req.method === 'OPTIONS') return res.status(200).end();
 
-  //if (req.method !== 'POST') {
-    //return res.status(405).json({ error: 'Método no permitido' });
-  //}
 
-  //const { name, email, message } = req.body;
 
-  //const transporter = nodemailer.createTransport({
-   // service: 'gmail',
-    //auth: {
-      //user: 'jesusveloz76@gmail.com',
-      //pass: 'atik oycj afzq ygjm',
-    //},
-  //});
 
-  //const mailOptions = {
-    //from: 'jesusveloz76@gmail.com',
-    //to: 'jesusveloz76@gmail.com',
-    //subject: `Mensaje de ${name}`,
-    //text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`,
-  //};
 
-  //try {
-    //await transporter.sendMail(mailOptions);
-    //res.status(200).json({ success: true });
-  //} catch (error) {
-    //res.status(500).json({ success: false, error: error.message });
-  //}
-//} 
+
+//código original
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método no permitido' });
+  }
+
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'jesusveloz76@gmail.com',
+      pass: 'atik oycj afzq ygjm',
+    },
+  });
+
+  const mailOptions = {
+    from: 'jesusveloz76@gmail.com',
+    to: 'jesusveloz76@gmail.com',
+    subject: `Mensaje de ${name}`,
+    text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+} 
 export default function handler(req, res) {
   res.status(200).json({ message: 'Funciona!' });
 }
