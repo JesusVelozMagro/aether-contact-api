5f35b87b8a79771dde46db2b2f7ce4d9918d9ff2
JesusVelozMagro <117239674+JesusVelozMagro@users.noreply.github.com>
2025-04-16 12:55:50 -0400

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,31 +1,37 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { name, email, message } = req.body;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'jesusveloz76@gmail.com',
-        pass: 'atik oycj afzq ygjm',
-      },
-    });
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
-    const mailOptions = {
-      from: 'jesusveloz76@gmail.com',
-      to: 'jesusveloz76@gmail.com',
-      subject: `Mensaje de ${name}`,
-      text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`,
-    };
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método no permitido' });
+  }
 
-    try {
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ success: true });
-    } catch (error) {
-      res.status(500).json({ success: false, error: error.message });
-    }
-  } else {
-    res.status(405).json({ error: 'Método no permitido' });
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'jesusveloz76@gmail.com',
+      pass: 'atik oycj afzq ygjm',
+    },
+  });
+
+  const mailOptions = {
+    from: 'jesusveloz76@gmail.com',
+    to: 'jesusveloz76@gmail.com',
+    subject: `Mensaje de ${name}`,
+    text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
   }
 }
