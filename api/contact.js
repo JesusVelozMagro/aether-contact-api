70f3d3976038f2b2df6f537bb6340671a57f8015
JesusVelozMagro <117239674+JesusVelozMagro@users.noreply.github.com>
2025-04-16 13:02:10 -0400

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,37 +1,40 @@
-import nodemailer from 'nodemailer';
+//import nodemailer from 'nodemailer';
 
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//export default async function handler(req, res) {
+  //res.setHeader("Access-Control-Allow-Origin", "*");
+  //res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  //res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  //if (req.method === 'OPTIONS') return res.status(200).end();
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Método no permitido' });
-  }
+  //if (req.method !== 'POST') {
+    //return res.status(405).json({ error: 'Método no permitido' });
+  //}
 
-  const { name, email, message } = req.body;
+  //const { name, email, message } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'jesusveloz76@gmail.com',
-      pass: 'atik oycj afzq ygjm',
-    },
-  });
+  //const transporter = nodemailer.createTransport({
+   // service: 'gmail',
+    //auth: {
+      //user: 'jesusveloz76@gmail.com',
+      //pass: 'atik oycj afzq ygjm',
+    //},
+  //});
 
-  const mailOptions = {
-    from: 'jesusveloz76@gmail.com',
-    to: 'jesusveloz76@gmail.com',
-    subject: `Mensaje de ${name}`,
-    text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`,
-  };
+  //const mailOptions = {
+    //from: 'jesusveloz76@gmail.com',
+    //to: 'jesusveloz76@gmail.com',
+    //subject: `Mensaje de ${name}`,
+    //text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`,
+  //};
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ success: true });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+  //try {
+    //await transporter.sendMail(mailOptions);
+    //res.status(200).json({ success: true });
+  //} catch (error) {
+    //res.status(500).json({ success: false, error: error.message });
+  //}
+//} 
+export default function handler(req, res) {
+  res.status(200).json({ message: 'Funciona!' });
 }
